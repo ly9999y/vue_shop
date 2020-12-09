cc016451abc54a2ea7c440f85de435eb0c412f03
ly9999y <478125686@qq.com>
2020-12-09 09:11:10 +0800

    login logout功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
